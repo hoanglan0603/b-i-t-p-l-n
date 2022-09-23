23eefeac6c5191d77bdc352f9431435a881a6950
Hoang Lan <hoanglan2k2vp@gmail.com>
2022-09-23 09:24:23 +0700

    Add file dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+a_dict = {'server': 'db.diveintopython3.org', 'database': 'mysql'} ①
+print(a_dict)
+# kq: {'server': 'db.diveintopython3.org', 'database': 'mysql'}
+print(a_dict['server']) ②
+ # kq :'db.diveintopython3.org'
+print(a_dict['database']) ③
+ # kq :'mysql'
+print(a_dict['db.diveintopython3.org'])
+# error vì ko lấy dc value để tìm key
+ # replace value of key
+    a_dict['database'] = 'blog'
+# add key-value
+  a_dict['user'] = 'dora'
+  # python có phân biêth hoa thường ở key
+  # nó vẫn thêm 1 cặp key-value
+  a_dict['User'] = 'mark'
+
+
+  # None là giá trị null, và nó tạo ra false
